8be674c87a7f53566e69896291160a1445f40e72
Robert Leon <llexi@hotmail.com>
2014-05-06 15:01:09 +0100

    App homepage is now loading at port 9393

--- MyApp.rb
+++ MyApp.rb
@@ -1,18 +1,17 @@
-require "ainatra/base"
-
-class MyApp < Sinatra::base
-	get "/" do
-		erb :index
-	end
-
-	post "/thankyou" do
-		@name = params["name"]
-		@email = params["email"]
-		erb :thankyou
-	end
-
-	get "/form" do
-		erb :form
-	end
+require "sinatra/base"
+ 
+class MyApp < Sinatra::Base
+    get "/" do
+        erb :index
+    end
+     
+    post "/thankyou" do
+        @name = params["name"]
+        @email = params["email"]
+        erb :thankyou
+    end
+     
+    get "/form" do
+        erb :form
+    end
 end
-
